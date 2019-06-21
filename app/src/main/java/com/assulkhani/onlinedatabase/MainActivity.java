9ae4023f1f402d89e31405ce1a827ce5ac102cd2
assulkhani <assulkhani08@gmail.com>
2019-06-21 18:23:04 +0700

    Update Menerapkan adapter dan layout manager

--- app/src/main/java/com/assulkhani/onlinedatabase/MainActivity.java
+++ app/src/main/java/com/assulkhani/onlinedatabase/MainActivity.java
@@ -2,6 +2,8 @@ package com.assulkhani.onlinedatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +11,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<MovieModel> dataMovie = new ArrayList<>();
+    RecyclerView recycler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        recycler = findViewById(R.id.recyclerView);
         // pertama membuat layout per item
         // kedua membuat model datanya
         // selanjutnya membuat dummy data
@@ -26,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             dataMovie.add(moviel);
         }
+
+        recycler.setAdapter(new MovieAdapter(MainActivity.this, dataMovie));
+        recycler.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+
 
     }
 }
